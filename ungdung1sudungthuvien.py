d3c71e7a4cc39490eb0896b33eaa832780bc33b6
TRANDAT2310 <115207964+TRANDAT2310@users.noreply.github.com>
2024-05-28 15:06:48 +0700

    Add files via upload

---
+++ ungdung1sudungthuvien.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+doc = pd.read_csv(r'D:/dữ liệu học máy, R/mall_chuan_hoa.csv')
+print(doc)
+
+
+# Lấy cột 'Annual Income (k$)' và 'Spending Score (1-100)'
+X = doc[['Thu nhập_Normalized', 'Chi tiêu_Normalized']]
+
+# Số lượng cụm
+k = 3
+
+# Sử dụng KMeans từ thư viện sklearn
+kmeans = KMeans(n_clusters=k, random_state=42)
+kmeans.fit(X)
+
+# Nhãn của các cụm
+labels = kmeans.labels_
+
+# Vị trí của các centroids
+centers = kmeans.cluster_centers_
+
+#tính MSE
+MSE = kmeans.inertia_/ len(X)
+print(f"Mean square error : {MSE}")
+
+
+
+# Vẽ biểu đồ phân cụm
+colors = ['r', 'g', 'b']
+for i in range(k):
+    plt.scatter(X.iloc[labels == i, 0], X.iloc[labels == i, 1], c=colors[i], label=f'Cluster {i+1}')
+plt.scatter(centers[:, 0], centers[:, 1], marker='.', s=300, c='black', label='centers')
+plt.xlabel('Mức thu nhập')
+plt.ylabel('Mức chi tiêu')
+plt.title('Phân cụm khách hàng')
+plt.legend()
+plt.show()
